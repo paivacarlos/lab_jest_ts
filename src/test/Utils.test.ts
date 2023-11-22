273c8b3175e0eb20f396e680b5c620b6221cec35
Carlos Paiva <paivakk22@gmail.com>
2023-11-22 20:47:01 -0300

    structure of unit test

--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -2,9 +2,16 @@ import { toUpperCase } from "../app/Utils"
 
 describe('Utils test suite', ()=> {
 
-    test('Should return uppercase', ()=>{
-        const result = toUpperCase('qwer')
-        expect(result).toBe('QWER')
+    it('Should return uppercase of valid string', ()=>{
+        //arrange - sut (system unit test)
+        const sut = toUpperCase
+        const expected = 'QWER'
+
+        //act
+        const actual = sut('qwer')
+
+        //assert
+        expect(actual).toBe(expected)
     })
 
 })
