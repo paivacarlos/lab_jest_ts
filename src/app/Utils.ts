034b538d3f6031bcf5a8c6e926057b630fa34566
Carlos Paiva <paivakk22@gmail.com>
2023-11-22 21:22:24 -0300

    jest assertions and matchers

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,3 +1,21 @@
 export function toUpperCase(value: string) {
     return value.toLocaleUpperCase();
 }
+
+export type stringInfo = {
+    lowerCase: string,
+    upperCase: string,
+    characters: string[],
+    length: number,
+    extraInfo: Object | undefined
+}
+
+export function getStringInfo(value: string): stringInfo{
+    return{
+        lowerCase: value.toLocaleLowerCase(),
+        upperCase: value.toLocaleUpperCase(),
+        characters: Array.from(value),
+        length: value.length,
+        extraInfo:{}
+    }
+}
